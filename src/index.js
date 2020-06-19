6f5708fd5807b943a6e7c9e78739ecd41b4390b4
Daniel M. Moreira <dm-moreira@hotmail.com>
2020-06-19 19:45:30 -0300

    update with handlerclick

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
     render() {
       return (
         <button 
           className="square" 
-          onClick={()=>{this.setState({value:'X'})}
+          onClick={()=>{this.props.onClick()}
         }>
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -28,10 +22,14 @@ class Square extends React.Component {
         squares: Array(9).fill(null),
       }
     }
+
     renderSquare(i) {
-      return <Square 
-        value={this.state.squares[i]}
-        onClick={()=>this.handleClick(i)}/>;
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={()=>this.handleClick(i)}
+        />
+      );
     }
   
     render() {
